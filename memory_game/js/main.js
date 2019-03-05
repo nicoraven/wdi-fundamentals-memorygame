0eac29f08ed44b451154d68efec23b0d575f384e
khyreerusydi <nicoraventbh@gmail.com>
2019-03-05 18:36:40 +0800

    created game logic (completed base game)

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -33,18 +33,26 @@ var checkForMatch = function() {
 };
 
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId].rank);
+	var cardId = this.getAttribute('data-id');
+	console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
 
 	cardsInPlay.push(cards[cardId].rank);
-
-	//if (cardsInPlay.length === 2) {
+	this.setAttribute("src", cards[cardId].cardImage)
+	if (cardsInPlay.length === 2) {
 		checkForMatch();
-	//}
+	}
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+	for (var i = 0; i < cards.length; i++) {
+    	var cardElement = document.createElement('img');
+    	cardElement.setAttribute("src", "images/back.png");
+    	cardElement.setAttribute('data-id', i);
+    	cardElement.addEventListener('click', flipCard);
+    	document.getElementById("game-board").appendChild(cardElement);
+}
+};
 
+createBoard();
 
